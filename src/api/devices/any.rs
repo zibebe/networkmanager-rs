9208e4a62d4f5237744759c58c09cb854931beb1
Tobias Tschinkowitz <tobias.tschinkowitz@icloud.com>
2020-06-08 23:01:49 +0200

    reordered any trait

--- src/api/devices/any.rs
+++ src/api/devices/any.rs
@@ -6,6 +6,78 @@ use crate::types::{Capability, ConnectivityState, DeviceInterfaceFlag, DeviceTyp
 use crate::Error;
 use num_traits::FromPrimitive;
 
+pub trait Any {
+    fn reapply(
+        &self,
+        connection: ::std::collections::HashMap<
+            &str,
+            ::std::collections::HashMap<&str, dbus::arg::Variant<Box<dyn dbus::arg::RefArg>>>,
+        >,
+        version_id: u64,
+        flags: u32,
+    ) -> Result<(), Error>;
+    fn get_applied_connection(
+        &self,
+        flags: u32,
+    ) -> Result<
+        (
+            ::std::collections::HashMap<
+                String,
+                ::std::collections::HashMap<
+                    String,
+                    dbus::arg::Variant<Box<dyn dbus::arg::RefArg + 'static>>,
+                >,
+            >,
+            u64,
+        ),
+        Error,
+    >;
+    fn disconnect(&self) -> Result<(), Error>;
+    fn delete(&self) -> Result<(), Error>;
+    fn udi(&self) -> Result<String, Error>;
+    fn interface(&self) -> Result<String, Error>;
+    fn ip_interface(&self) -> Result<String, Error>;
+    fn driver(&self) -> Result<String, Error>;
+    fn driver_version(&self) -> Result<String, Error>;
+    fn firmware_version(&self) -> Result<String, Error>;
+    fn capabilities(&self) -> Result<Capability, Error>;
+    fn ip4_address(&self) -> Result<u32, Error>;
+    fn state(&self) -> Result<u32, Error>;
+    fn state_reason(&self) -> Result<(u32, u32), Error>;
+    fn active_connection(&self) -> Result<Connection, Error>;
+    fn ip4_config(&self) -> Result<Config, Error>;
+    fn dhcp4_config(&self) -> Result<Config, Error>;
+    fn ip6_config(&self) -> Result<Config, Error>;
+    fn dhcp6_config(&self) -> Result<Config, Error>;
+    fn managed(&self) -> Result<bool, Error>;
+    fn set_managed(&self, value: bool) -> Result<(), Error>;
+    fn autoconnect(&self) -> Result<bool, Error>;
+    fn set_autoconnect(&self, value: bool) -> Result<(), Error>;
+    fn firmware_missing(&self) -> Result<bool, Error>;
+    fn nm_plugin_missing(&self) -> Result<bool, Error>;
+    fn device_type(&self) -> Result<DeviceType, Error>;
+    fn available_connections(&self) -> Result<Vec<Connection>, Error>;
+    fn physical_port_id(&self) -> Result<String, Error>;
+    fn mtu(&self) -> Result<u32, Error>;
+    fn metered(&self) -> Result<u32, Error>;
+    fn lldp_neighbors(
+        &self,
+    ) -> Result<
+        Vec<
+            ::std::collections::HashMap<
+                String,
+                dbus::arg::Variant<Box<dyn dbus::arg::RefArg + 'static>>,
+            >,
+        >,
+        Error,
+    >;
+    fn real(&self) -> Result<bool, Error>;
+    fn ip4_connectivity(&self) -> Result<ConnectivityState, Error>;
+    fn ip6_connectivity(&self) -> Result<ConnectivityState, Error>;
+    fn interface_flags(&self) -> Result<DeviceInterfaceFlag, Error>;
+    fn hw_address(&self) -> Result<String, Error>;
+}
+
 macro_rules! impl_any {
     ($name:ty, $lifetime:tt) => {
         impl<$lifetime> Any for $name {
@@ -172,78 +244,6 @@ macro_rules! impl_any {
             }
         }
     };
-}
-
-pub trait Any {
-    fn reapply(
-        &self,
-        connection: ::std::collections::HashMap<
-            &str,
-            ::std::collections::HashMap<&str, dbus::arg::Variant<Box<dyn dbus::arg::RefArg>>>,
-        >,
-        version_id: u64,
-        flags: u32,
-    ) -> Result<(), Error>;
-    fn get_applied_connection(
-        &self,
-        flags: u32,
-    ) -> Result<
-        (
-            ::std::collections::HashMap<
-                String,
-                ::std::collections::HashMap<
-                    String,
-                    dbus::arg::Variant<Box<dyn dbus::arg::RefArg + 'static>>,
-                >,
-            >,
-            u64,
-        ),
-        Error,
-    >;
-    fn disconnect(&self) -> Result<(), Error>;
-    fn delete(&self) -> Result<(), Error>;
-    fn udi(&self) -> Result<String, Error>;
-    fn interface(&self) -> Result<String, Error>;
-    fn ip_interface(&self) -> Result<String, Error>;
-    fn driver(&self) -> Result<String, Error>;
-    fn driver_version(&self) -> Result<String, Error>;
-    fn firmware_version(&self) -> Result<String, Error>;
-    fn capabilities(&self) -> Result<Capability, Error>;
-    fn ip4_address(&self) -> Result<u32, Error>;
-    fn state(&self) -> Result<u32, Error>;
-    fn state_reason(&self) -> Result<(u32, u32), Error>;
-    fn active_connection(&self) -> Result<Connection, Error>;
-    fn ip4_config(&self) -> Result<Config, Error>;
-    fn dhcp4_config(&self) -> Result<Config, Error>;
-    fn ip6_config(&self) -> Result<Config, Error>;
-    fn dhcp6_config(&self) -> Result<Config, Error>;
-    fn managed(&self) -> Result<bool, Error>;
-    fn set_managed(&self, value: bool) -> Result<(), Error>;
-    fn autoconnect(&self) -> Result<bool, Error>;
-    fn set_autoconnect(&self, value: bool) -> Result<(), Error>;
-    fn firmware_missing(&self) -> Result<bool, Error>;
-    fn nm_plugin_missing(&self) -> Result<bool, Error>;
-    fn device_type(&self) -> Result<DeviceType, Error>;
-    fn available_connections(&self) -> Result<Vec<Connection>, Error>;
-    fn physical_port_id(&self) -> Result<String, Error>;
-    fn mtu(&self) -> Result<u32, Error>;
-    fn metered(&self) -> Result<u32, Error>;
-    fn lldp_neighbors(
-        &self,
-    ) -> Result<
-        Vec<
-            ::std::collections::HashMap<
-                String,
-                dbus::arg::Variant<Box<dyn dbus::arg::RefArg + 'static>>,
-            >,
-        >,
-        Error,
-    >;
-    fn real(&self) -> Result<bool, Error>;
-    fn ip4_connectivity(&self) -> Result<ConnectivityState, Error>;
-    fn ip6_connectivity(&self) -> Result<ConnectivityState, Error>;
-    fn interface_flags(&self) -> Result<DeviceInterfaceFlag, Error>;
-    fn hw_address(&self) -> Result<String, Error>;
 }
 
 impl_any!(WiFiDevice<'a>, 'a);
