1e15facf412b86b129caf4a9f347d0915195abef
Tobias Tschinkowitz <tobias.tschinkowitz@icloud.com>
2020-06-14 13:13:28 +0100

    implemented dhcp4 and dhcp6config

--- src/configs/dhcp4.rs
+++ src/configs/dhcp4.rs
@@ -1,10 +1,36 @@
 use crate::dbus_api::DBusAccessor;
+use crate::errors::Error;
+use crate::gen::OrgFreedesktopNetworkManagerDHCP4Config;
 
 pub struct Dhcp4Config<'a> {
     dbus_accessor: DBusAccessor<'a>,
 }
 
-pub trait Dhcp4 {}
+pub trait Dhcp4 {
+    fn options(
+        &self,
+    ) -> Result<
+        ::std::collections::HashMap<
+            String,
+            dbus::arg::Variant<Box<dyn dbus::arg::RefArg + 'static>>,
+        >,
+        Error,
+    >;
+}
+
+impl<'a> Dhcp4 for Dhcp4Config<'a> {
+    fn options(
+        &self,
+    ) -> Result<
+        std::collections::HashMap<
+            String,
+            dbus::arg::Variant<std::boxed::Box<(dyn dbus::arg::RefArg + 'static)>>,
+        >,
+        Error,
+    > {
+        Ok(proxy!(self).options()?)
+    }
+}
 
 impl<'a> Dhcp4Config<'a> {
     pub(crate) fn new(dbus_accessor: DBusAccessor<'a>) -> Self {
--- src/configs/dhcp6.rs
+++ src/configs/dhcp6.rs
@@ -1,13 +1,39 @@
 use crate::dbus_api::DBusAccessor;
+use crate::errors::Error;
+use crate::gen::OrgFreedesktopNetworkManagerDHCP6Config;
 
 pub struct Dhcp6Config<'a> {
     dbus_accessor: DBusAccessor<'a>,
 }
 
-pub trait Dhcp6 {}
+pub trait Dhcp6 {
+    fn options(
+        &self,
+    ) -> Result<
+        ::std::collections::HashMap<
+            String,
+            dbus::arg::Variant<Box<dyn dbus::arg::RefArg + 'static>>,
+        >,
+        Error,
+    >;
+}
 
 impl<'a> Dhcp6Config<'a> {
     pub(crate) fn new(dbus_accessor: DBusAccessor<'a>) -> Self {
         Dhcp6Config { dbus_accessor }
+    }
+}
+
+impl<'a> Dhcp6 for Dhcp6Config<'a> {
+    fn options(
+        &self,
+    ) -> Result<
+        std::collections::HashMap<
+            String,
+            dbus::arg::Variant<std::boxed::Box<(dyn dbus::arg::RefArg + 'static)>>,
+        >,
+        Error,
+    > {
+        Ok(proxy!(self).options()?)
     }
 }
